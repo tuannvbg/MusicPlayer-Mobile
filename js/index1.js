65ced25c8ffcef9ea598472994f4384b708cd4a1
Skye Sun <try_sun@outlook.com>
2017-02-21 22:08:02 +0800

    增加事件绑定

--- js/index1.js
+++ js/index1.js
@@ -31,7 +31,9 @@ var playSwitch = function() {
             // 添加过渡动画
             addTransition(true, playButton)
 
-        } else {
+            updateTime()
+// (bug) 播放和暂停功能与列表播放的事件有冲突
+        } else if(musicPlayer.played) {
             musicPlayer.pause()
             log('paused')
             // 切换播放图标
@@ -89,6 +91,104 @@ var addTransition = function(flag, button) {
     transition("#id-button-next", 'button-next_play')
 }
 
+// 显示歌曲的 当前时间 和 总时间
+var showTime = function() {
+    var spanDura = getElement('#id-span-duration')
+    var spanCurr = getElement('#id-span-currentTime')
+    // 获取当前歌曲的总时间并将其写入 duration
+    // (利用 oncanplay 已解决)这句语句存在 bug， 当 mp3 文件没有被加载时无法获取到 duration 属性
+    var duration = parseInt(musicPlayer.duration)
+    setTime(duration, spanDura)
+    // 获取歌曲的当前时间并将其写入 currentTime
+    var currTime = parseInt(musicPlayer.currentTime)
+    setTime(currTime, spanCurr)
+
+}
+var updateTime = function() {
+    var spanCurr = getElement('#id-span-currentTime')
+    var spanDura = getElement('#id-span-duration')
+    var progress = getElement('#id-input-progress')
+    var duration = parseInt(musicPlayer.duration)
+    log('duration', duration)
+    // 进度条与时间同步
+    var a = setInterval(function(){
+        var currTime = parseInt(musicPlayer.currentTime)
+        var value = currTime / duration
+        // log('value: ', value)
+        // progress.value = value
+        setTime(currTime, spanCurr)
+    }, 1000)
+    setTimeout(function(){
+        clearInterval(a)
+    }, duration * 1000)
+}
+
+var setTime = function(time, container) {
+    var Min = parseInt(time / 60)
+    var Sec = time % 60
+    Min  = rjust(String(Min), 2, '0')
+    Sec  = rjust(String(Sec), 2, '0')
+    container.innerHTML = `${Min}:${Sec}`
+}
+// 补全不足的 0 的函数
+var rjust = function(s, width, fillchar=' ') {
+    var result = ''
+    // 比较 s 与 width 的长度
+    if(s.length >= width) {
+        result = s
+    } else {
+        for (var i = 0; i < (width - s.length); i++) {
+            result = result + fillchar
+        }
+        result = result + s
+    }
+    return result
+}
+
+var playList = function() {
+    var selector = '.mp3-list'
+    var divName = getElement('#id-div-currentSong')
+    var songImg = getElement(".skye-mp3-img")
+    bindEventAll(selector, 'click', function(event){
+        var self = event.target
+        var pathNum = self.dataset.path
+        var path = `res/mp3/${pathNum}.mp3`
+        var imgPath = `res/imgs/${pathNum}.jpg`
+        var name = self.querySelector('.mp3-list-name').innerHTML
+        musicPlayer.src = path
+        songImg.src = imgPath
+        divName.innerHTML = name
+        // musicPlayer.canplay = updateTime()
+        musicPlayer.play()
+    })
+}
+
+// 列表循环时存在 bug
+var playLoop = function() {
+    // 得到列表中所有音乐的路径并存入数组
+    var list = document.querySelectorAll('.mp3-list')
+    var paths = []
+    var imgPaths = []
+    for (var i = 0; i < list.length; i++) {
+        var pathNum = list[i].dataset.path
+        var path = `res/mp3/${pathNum}.mp3`
+        var imgPath = `res/imgs/${pathNum}.jpg`
+        paths.push(path)
+        imgPaths.push(imgPath)
+    }
+    //
+    bindEvent(musicPlayer, 'ended', function(){
+        var songImg = getElement(".skye-mp3-img")
+        var currSrc = musicPlayer.src
+        var currNum = parseInt(currSrc.slice(currSrc.length - 5, currSrc.length - 4))
+        musicPlayer.src = paths[currNum % list.length]
+        songImg.src = imgPaths[currNum % list.length]
+        musicPlayer.canplay = updateTime()
+        musicPlayer.play()
+    })
+}
+
+
 
 
 
@@ -105,5 +205,8 @@ var addTransition = function(flag, button) {
 
 var __skye = function() {
     playSwitch()
+    showTime()
+    playList()
+    playLoop()
 }
 // __skye()
